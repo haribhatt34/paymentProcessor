ffdae155130bbb1072544bf87a8b4489ee57898b
unknown <comviva@MCKL-TR1.COMVIVA.COM>
2019-08-07 17:27:21 +0530

    initial commit

---
+++ src/main/java/com/example/demo/PaymentProcessorGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentProcessorGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentProcessorGitApplication.class, args);
+	}
+
+}
